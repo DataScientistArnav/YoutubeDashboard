44091d5bddcd33f167fd9a4603fb623e737cc4cc
Arnav Goel <ag4273@drexel.edu>
2024-07-09 03:09:06 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# YouTube API key
+API_KEY = 'AIzaSyC6GtYINJN4ELMAQDNQwqQoVh4QwJIhC9o'
+
+# Database configuration
+DB_USER = 'root'
+DB_PASSWORD = 'Arnav@tu1'
+DB_NAME = 'YoutubeData'
+DB_HOST = 'localhost'
+DB_PORT = 3306
